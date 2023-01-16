1126868114235494dfbe9decd6affd54a2c753b5
e-nagornyak <dolkiv@gmail.com>
2023-01-16 23:01:40 +0200

    Added ErrorSnackbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import ButtonAppBar from "../components/ButtonAppBar/ButtonAppBar";
 import Container from '@mui/material/Container';
 import {TodolistsList} from "../features/TodolistsList/TodolistsList";
-
+import LinearProgress from '@mui/material/LinearProgress';
 
 function App() {
     return (
         <div className="App">
             <ButtonAppBar/>
+            <LinearProgress color="info" />
             <Container fixed>
                 <TodolistsList/>
             </Container>
